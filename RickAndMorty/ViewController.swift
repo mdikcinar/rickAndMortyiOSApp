d02fcf8ee8a79140cd49f570d338ecbd107bd476
Mustafa Ali Dikçinar <mustafaalidikcinar@hotmail.com>
2023-06-07 20:22:15 +0300

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Mustafa Ali Dikcinar on 7.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
